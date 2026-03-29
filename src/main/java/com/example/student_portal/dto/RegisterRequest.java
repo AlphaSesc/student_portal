d8b046620799a8e5301b1caa4f8ebd450f73d111
Alpha Sesc <anuz.g12345@gmail.com>
2026-03-29 13:52:51 +0545

    update: changed registerrequest dto to handle validation

--- src/main/java/com/example/student_portal/dto/RegisterRequest.java
+++ src/main/java/com/example/student_portal/dto/RegisterRequest.java
@@ -1,6 +1,10 @@
 package com.example.student_portal.dto;
 
 import com.example.student_portal.entity.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +12,14 @@ import lombok.Setter;
 @Setter
 public class RegisterRequest {
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email format is invalid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
+    @NotNull(message = "Role is required")
     private UserRole role;
 }
