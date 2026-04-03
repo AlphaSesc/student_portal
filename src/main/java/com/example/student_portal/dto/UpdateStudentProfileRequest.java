5013e39077a2b909e761e44f4d2478f8c6e8575f
Alpha Sesc <anuz.g12345@gmail.com>
2026-04-04 00:19:19 +0545

    feat:Added Validation in UpdateStudentProfileRequest

--- src/main/java/com/example/student_portal/dto/UpdateStudentProfileRequest.java
+++ src/main/java/com/example/student_portal/dto/UpdateStudentProfileRequest.java
@@ -1,5 +1,6 @@
 package com.example.student_portal.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter
@@ -9,8 +10,15 @@ import lombok.*;
 @Builder
 public class UpdateStudentProfileRequest {
 
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     private String firstName;
+
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
     private String lastName;
+
+    @Pattern(regexp = "^[0-9]{7,15}$", message = "Phone must be valid")
     private String phone;
+
+    @Size(max = 500, message = "Address too long")
     private String address;
 }
