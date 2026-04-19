6ad9f7c2e61c424e37d2a48f160e98d2a5f110d3
Alpha Sesc <anuz.g12345@gmail.com>
2026-04-19 16:26:54 +0545

    feat:Added PayInvoiceRequest

---
+++ src/main/java/com/example/student_portal/dto/finance/PayInvoiceRequest.java
@@ -0,0 +1,13 @@
+package com.example.student_portal.dto.finance;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PayInvoiceRequest {
+
+    private String reference;
+}
