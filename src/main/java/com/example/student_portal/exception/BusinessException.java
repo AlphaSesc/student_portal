40c396f5fa0ab2074a4b24816f871708e3b5c03b
Alpha Sesc <anuz.g12345@gmail.com>
2026-04-22 13:38:26 +0545

    update:minor fix

--- src/main/java/com/example/student_portal/exception/BusinessException.java
+++ src/main/java/com/example/student_portal/exception/BusinessException.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-// Base class for all business-related exceptions in the Finance service.
+// Base class for all business-related exceptions in the Student service.
 // Allows consistent error handling by associating each exception with an HTTP status.
 public abstract class BusinessException extends RuntimeException {
 
